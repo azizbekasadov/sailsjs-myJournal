0ccb0a6c6ec66bc0322db8a3c12ef2207f5dbce7
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-09-26 18:18:17 +0200

    rewritten routes with HTTP Requests

--- my_journal/config/routes.js
+++ my_journal/config/routes.js
@@ -1,6 +1,9 @@
 module.exports.routes = {
   '/': { view: 'pages/homepage' },
-  '/posts': 'PostController.posts', //{ view: 'PostController.posts' },
-  '/findById/:postId': 'PostController.findById',
-  '/create':'PostController.create'
+  'GET /posts': 'PostController.posts', //{ view: 'PostController.posts' },
+  'GET /post/:postId': 'PostController.findById',
+  'POST /post': 'PostController.create',
+  'DELETE /post/:postId': 'PostController.delete',
 };
+
+// HTTP Method => POST 
