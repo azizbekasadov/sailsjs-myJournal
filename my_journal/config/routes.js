9124737b4e1c64d6f3a57f52af4a5de1c4bed8f5
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-09-27 12:06:56 +0200

    added home page with posts and basic CRUD operations via REST API

--- my_journal/config/routes.js
+++ my_journal/config/routes.js
@@ -1,9 +1,10 @@
 module.exports.routes = {
   '/': { view: 'pages/homepage' },
-  'GET /posts': 'PostController.posts', //{ view: 'PostController.posts' },
+  'GET /posts': 'PostController.posts',
   'GET /post/:postId': 'PostController.findById',
   'POST /post': 'PostController.create',
   'DELETE /post/:postId': 'PostController.delete',
+  'GET /home': 'post/home'
 };
 
 // HTTP Method => POST 
